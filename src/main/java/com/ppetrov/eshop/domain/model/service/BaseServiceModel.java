ac7c70d8a342d71a245b2b6985257a13feb83f6f
om <om@localhost.localdomain>
2019-05-02 04:29:45 +0300

    Add models

---
+++ src/main/java/com/ppetrov/eshop/domain/model/service/BaseServiceModel.java
@@ -0,0 +1,14 @@
+package com.ppetrov.eshop.domain.model.service;
+
+public abstract class BaseServiceModel {
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
