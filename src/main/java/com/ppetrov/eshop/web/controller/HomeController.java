602701e0a6115f00dd8949c9163801d8ebb3c529
om <om@localhost.localdomain>
2019-05-03 03:48:49 +0300

    fix: path changes

--- src/main/java/com/ppetrov/eshop/web/controller/HomeController.java
+++ src/main/java/com/ppetrov/eshop/web/controller/HomeController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.security.Principal;
+
 @Controller
 public class HomeController extends BaseController{
 
@@ -12,5 +14,13 @@ public class HomeController extends BaseController{
         return view("index");
     }
 
-    
+
+    @GetMapping("/home")
+    public ModelAndView home(Principal principal, ModelAndView modelAndView){
+        modelAndView.addObject("username", principal.getName());
+        return view("user-home", modelAndView);
+    }
+
+
+
 }
--- src/main/java/com/ppetrov/eshop/web/controller/UserController.java
+++ src/main/java/com/ppetrov/eshop/web/controller/UserController.java
@@ -1,31 +1,49 @@
 package com.ppetrov.eshop.web.controller;
 
+import com.ppetrov.eshop.domain.model.binding.UserRegisterBindingModel;
+import com.ppetrov.eshop.domain.model.service.UserServiceModel;
+import com.ppetrov.eshop.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/users")
 public class UserController extends BaseController {
+    private final UserService userService;
 
-//
-//    private final ModelMapper modelMapper;
-//
-//    @Autowired
-//    public UserController(ModelMapper modelMapper){
-//        this.modelMapper = modelMapper;
-//    }
+    private final ModelMapper modelMapper;
 
-
-    public UserController() {
+    @Autowired
+    public UserController(UserService userService, ModelMapper modelMapper) {
+        this.userService = userService;
+        this.modelMapper = modelMapper;
     }
 
     @GetMapping("/login")
-    public ModelAndView login(){
-        return view("login");
+    public ModelAndView login() {
+        return this.view("login");
     }
 
     @GetMapping("/register")
-    public ModelAndView register(){
-        return view(("register"));
+    public ModelAndView register() {
+        return this.view("register");
+    }
+
+    @PostMapping("/register")
+    public ModelAndView registerConfirm(@ModelAttribute UserRegisterBindingModel userRegisterBindingModel) {
+        if(!userRegisterBindingModel.getPassword()
+                .equals(userRegisterBindingModel.getConfirmPassword())) {
+            return this.view("register");
+        }
+
+        this.userService.registerUser(this.modelMapper.map(userRegisterBindingModel, UserServiceModel.class));
+
+        return this.redirect("/users/login");
     }
 }
