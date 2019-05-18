83a813559a9b5cf98589040bba94d6f653427aec
om <om@localhost.localdomain>
2019-05-18 12:23:46 +0300

    Fix Product fuctionality

--- src/main/java/com/ppetrov/eshop/domain/entity/Product.java
+++ src/main/java/com/ppetrov/eshop/domain/entity/Product.java
@@ -1,4 +1,72 @@
 package com.ppetrov.eshop.domain.entity;
 
-public class Product {
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Entity
+@Table(name = "products")
+public class Product extends BaseEntity{
+
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private String imageUrl;
+    private Set<Category> categories;
+
+    @Column(name = "product_name", nullable = false)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "description", nullable = false)
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Column(name = "price", nullable = false)
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Column(name = "image_url")
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @ManyToMany(targetEntity = Category.class)
+    @JoinTable(
+            name = "products_categories",
+            joinColumns = @JoinColumn(
+                    name = "product_id",
+                    referencedColumnName = "id"
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "category_id",
+                    referencedColumnName = "id"
+            )
+    )
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
 }
