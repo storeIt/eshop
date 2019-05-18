3aacc974e1be0dd62361e3c1915532c9d0ad7945
om <om@localhost.localdomain>
2019-05-18 12:10:46 +0300

    Add view resources

---
+++ src/main/java/com/ppetrov/eshop/domain/model/view/ProductAllViewModel.java
@@ -0,0 +1,43 @@
+package com.ppetrov.eshop.domain.model.view;
+
+import java.math.BigDecimal;
+
+public class ProductAllViewModel {
+
+    private String id;
+    private String name;
+    private BigDecimal price;
+    private String imageUrl;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
